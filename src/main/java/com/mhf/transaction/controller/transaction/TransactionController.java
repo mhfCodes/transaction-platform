71601165d98769db52ee9f644631ca75de5a5067
mhfCodes <mohammadhossein.farrokhi.78@gmail.com>
2026-08-10 11:54:13 +0330

    initialized main controllers

---
+++ src/main/java/com/mhf/transaction/controller/transaction/TransactionController.java
@@ -0,0 +1,9 @@
+package com.mhf.transaction.controller.transaction;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/transactions")
+public class TransactionController {
+}
